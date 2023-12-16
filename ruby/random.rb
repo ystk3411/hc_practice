173259bfa317884c3ec1d258aeb6085210a7ffe1
ystk3411 <yoshi.hama0701@gmail.comgit push -u origin main>
2023-12-16 23:46:39 +0900

    rubocopを実行

--- ruby/random.rb
+++ ruby/random.rb
@@ -1,7 +1,7 @@
-members = ["A","B","C","D","E","F"]
-index_array = [[2,4],[3,3]]
+members = %w[A B C D E F]
+index_array = [[2, 4], [3, 3]]
 shuffled_members = members.shuffle
 shuffled_index_array = index_array.shuffle
-group1 = shuffled_members.slice(0,shuffled_index_array[0][0])
-group2 = shuffled_members.slice(shuffled_index_array[0][0],shuffled_index_array[0][1])
-puts  group1.sort.to_s,group2.sort.to_s
+group1 = shuffled_members.slice(0, shuffled_index_array[0][0])
+group2 = shuffled_members.slice(shuffled_index_array[0][0], shuffled_index_array[0][1])
+puts group1.sort, group2.sort
